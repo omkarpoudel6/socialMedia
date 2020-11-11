9a71f22b0bb4e1bcd7ad3dd1798c1ebd10e38092
omkarpoudel6 <omkar.poudel77@gmail.com>
2020-11-11 01:32:30 -0800

    Comments in posts started

--- SocialMedia/posts/urls.py
+++ SocialMedia/posts/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from .views import userPost,like_post
+from .views import userPost,like_post,comment_post
 from django.conf.urls.static import static
 
 app_name='posts'
 
 urlpatterns = [
     path('', userPost,name='userpost'),
-    path('like/',like_post,name='like-post')
+    path('like/',like_post,name='like-post'),
+    path('comment/',comment_post,name='comment-post')
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
